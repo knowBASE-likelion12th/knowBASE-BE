42a2b38619d05b7acf2ce6eb1537b6169f85ba3c
JuyeonLee <dlwndus708@naver.com>
2024-05-24 01:25:38 +0900

    ⚡️ Post_id 기본키 생성 전략 변경
    호출시 기본키가  50씩 증가하지 않고 1씩 증가하도록 수정

--- src/main/java/com/knowbase/knowbase/domain/Post.java
+++ src/main/java/com/knowbase/knowbase/domain/Post.java
@@ -12,10 +12,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@SequenceGenerator(
+        name = "POST_ID", //식별자 생성기 이름
+        sequenceName = "POST_ID_SEQ", //DB에 등록되어 있는 Sequence 이름
+        initialValue = 1, //처음 시작 value 설정
+        allocationSize = 1 //Sequence 한번 호출 시 증가하는 수
+        //allocationSize가 기본값이 50이므로 1로 설정하지 않을 시, sequence 호출 시 마다 50씩 증가
+)
 @Table(name="POSTS")
 public class Post extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "POST_ID"
+    )
     @Column(name="POST_ID")
     private Long postId;
 
