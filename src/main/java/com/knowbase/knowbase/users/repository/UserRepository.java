739614185cce6fc451351445009992d29f4fdde1
rtgc0915 <rtgc0915@gmail.com>
2024-05-23 16:20:49 +0900

    Style: 코드 변경 실수한 부분 복구

--- src/main/java/com/knowbase/knowbase/users/repository/UserRepository.java
+++ src/main/java/com/knowbase/knowbase/users/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String userId);
+    Optional<User> findByUserName(String userName);
 
 
     //모든 멘토 조회: 회원의 isMentor가 true(멘토)인 회원을 찾는 repository 메소드
