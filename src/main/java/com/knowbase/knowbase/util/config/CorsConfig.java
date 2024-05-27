ec1955836bbe6e28d1c8213152224f1e460297bb
rtgc0915 <rtgc0915@gmail.com>
2024-05-28 03:39:07 +0900

    FEAT: CORS 에러 코드 추가

---
+++ src/main/java/com/knowbase/knowbase/util/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.knowbase.knowbase.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
+
