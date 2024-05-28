b47656316fb0140c650d8687afa78af1abdea411
rtgc0915 <rtgc0915@gmail.com>
2024-05-28 23:14:18 +0900

    Update CorsConfig.java

--- src/main/java/com/knowbase/knowbase/util/config/CorsConfig.java
+++ src/main/java/com/knowbase/knowbase/util/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
 }
