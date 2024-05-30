b3e600cbb954d3aae06ac1d58acac459b398e45f
rtgc0915 <rtgc0915@gmail.com>
2024-05-31 00:28:13 +0900

    Update SecurityConfig.java

--- src/main/java/com/knowbase/knowbase/util/config/SecurityConfig.java
+++ src/main/java/com/knowbase/knowbase/util/config/SecurityConfig.java
@@ -1,24 +1,28 @@
-package com.knowbase.knowbase.util.config;
+    package com.knowbase.knowbase.util.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+    import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+    @Configuration
+    public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/api/**").permitAll()
-                )
-                .build();
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+            return http
+                    .cors(AbstractHttpConfigurer::disable)
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .formLogin(AbstractHttpConfigurer::disable)
+                    .headers((headerConfig) ->
+                            headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                    )
+                    .authorizeHttpRequests(authorizeRequests ->
+                            authorizeRequests
+                                    .requestMatchers("/api/**").permitAll()
+                    )
+                    .build();
+        }
     }
-}
