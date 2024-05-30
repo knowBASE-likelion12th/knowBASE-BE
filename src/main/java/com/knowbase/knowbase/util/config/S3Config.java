8f56dd9a9bb6fc8618efd482b8c0938d85ea216f
rtgc0915 <rtgc0915@gmail.com>
2024-05-31 01:42:17 +0900

    Update S3Config.java

--- src/main/java/com/knowbase/knowbase/util/config/S3Config.java
+++ src/main/java/com/knowbase/knowbase/util/config/S3Config.java
@@ -2,7 +2,7 @@ package com.knowbase.knowbase.util.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,11 +21,10 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3Client() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return (AmazonS3Client) AmazonS3ClientBuilder
-                .standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
